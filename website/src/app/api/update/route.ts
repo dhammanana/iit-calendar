4dff392b44822add1fe300801f61dac1cf0762e2
chathura <chathurawidanage@gmail.com>
2026-07-29 21:48:32 +0930

    revert

--- website/src/app/api/update/route.ts
+++ website/src/app/api/update/route.ts
@@ -45,10 +45,10 @@ export async function POST(request: NextRequest) {
           return false;
         }
 
-        // Temporarily ignore -dev / pre-releases check and allow all channels/dev releases
-        // if (channel === 'stable' && isDevRelease) {
-        //   return false;
-        // }
+        // If client is on stable channel, skip dev / pre-releases
+        if (channel === 'stable' && isDevRelease) {
+          return false;
+        }
 
         return true;
       });
